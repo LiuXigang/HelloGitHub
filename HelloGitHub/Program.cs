5d46e582ed03531b9a451f3f61dab69177f9c001
榴莲 <xigang.liu@51hgp.com>
2019-03-23 21:26:20 +0800

    HelloGitHub

--- HelloGitHub/Program.cs
+++ HelloGitHub/Program.cs
@@ -2,11 +2,11 @@
 
 namespace HelloGitHub
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello GitHub!");
         }
     }
 }
